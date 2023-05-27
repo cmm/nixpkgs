8961d58ffeb0960a2d15358e5984253b1163219a
Michael Livshin <repo@cmm.kakpryg.net>
2023-05-27 09:29:51 +0300

    mkshell: amend MANPATH and INFOPATH according to inputs

--- pkgs/build-support/mkshell/default.nix
+++ pkgs/build-support/mkshell/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   stdenv,
+  buildPackages,
 }:
 # A special kind of derivation that is only meant to be consumed by the
 # nix-shell.
@@ -32,17 +33,72 @@ lib.extendMkDerivation {
           # 3. filter out of the result everything that's in `inputsFrom` itself
           # this leaves actual dependencies of the derivations in `inputsFrom`, but never the derivations themselves
           (lib.subtractLists inputsFrom (lib.flatten (lib.catAttrs name inputsFrom)));
+
+      merged = {
+        buildInputs = mergeInputs "buildInputs";
+        nativeBuildInputs = packages ++ (mergeInputs "nativeBuildInputs");
+        propagatedBuildInputs = mergeInputs "propagatedBuildInputs";
+        propagatedNativeBuildInputs = mergeInputs "propagatedNativeBuildInputs";
+      };
+
+      docPathShellHook =
+        let
+          unwrapCC = pkg: if pkg ? cc then unwrapCC pkg.cc else pkg;
+          docInputs =
+            builtins.concatLists (builtins.attrValues merged)
+            # grab the unwrapped cc, as wrappers may or may not
+            # retain doc outputs
+            ++ (lib.optional (stdenv ? cc) (unwrapCC stdenv.cc))
+            ++ stdenv.defaultBuildInputs
+            ++ stdenv.defaultNativeBuildInputs
+            ++ stdenv.initialPath;
+          docDrvs = lib.unique (builtins.filter lib.isDerivation docInputs);
+          docPaths =
+            type:
+            lib.pipe docDrvs [
+              (map (lib.getOutput type))
+              (map (out: builtins.toPath "${out}/share/${type}"))
+              (builtins.filter builtins.pathExists)
+            ];
+          infoPaths = docPaths "info";
+          manPaths = docPaths "man";
+
+          # some packages neglect to build an info dir file
+          infoDirAddenda = stdenv.mkDerivation {
+            inherit (stdenv) system;
+            name = "${name}-info-dir-addenda";
+            passAsFile = [ "buildCommand" ];
+            buildCommand = ''
+              shopt -s nullglob
+              mkdir -p $out/share/info
+              for path in ${builtins.concatStringsSep " " infoPaths}; do
+                [[ -s $path/dir ]] && continue || :
+                for file in $path/*.info{,.gz}; do
+                  ${buildPackages.texinfo}/bin/install-info $file $out/share/info/dir
+                done
+              done
+            '';
+          };
+        in
+        ''
+          export INFOPATH=${
+            builtins.concatStringsSep ":" (infoPaths ++ [ (builtins.toPath "${infoDirAddenda}/share/info") ])
+          }:$INFOPATH
+          export MANPATH=${builtins.concatStringsSep ":" manPaths}:$MANPATH
+        '';
     in
     {
       inherit name;
 
-      buildInputs = mergeInputs "buildInputs";
-      nativeBuildInputs = packages ++ (mergeInputs "nativeBuildInputs");
-      propagatedBuildInputs = mergeInputs "propagatedBuildInputs";
-      propagatedNativeBuildInputs = mergeInputs "propagatedNativeBuildInputs";
+      inherit (merged)
+        buildInputs
+        nativeBuildInputs
+        propagatedBuildInputs
+        propagatedNativeBuildInputs
+        ;
 
       shellHook = lib.concatStringsSep "\n" (
-        lib.catAttrs "shellHook" (lib.reverseList inputsFrom ++ [ attrs ])
+        (lib.catAttrs "shellHook" (lib.reverseList inputsFrom ++ [ attrs ])) ++ [ docPathShellHook ]
       );
 
       phases = attrs.phases or [ "buildPhase" ];
