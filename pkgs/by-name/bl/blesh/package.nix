ec16b420a3651aca2d5139151bebe2e7f2a1156d
Michael Livshin <repo@cmm.kakpryg.net>
2024-03-11 23:05:12 +0200

    blesh: 0.4.0-devel3 -> 0.4.0-unstable-2026-08-12

--- pkgs/by-name/bl/blesh/package.nix
+++ pkgs/by-name/bl/blesh/package.nix
@@ -1,27 +1,44 @@
 {
   lib,
   stdenvNoCC,
-  fetchzip,
+  fetchFromGitHub,
   runtimeShell,
   bashInteractive,
   glibcLocales,
+  less,
+  ps,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation {
   pname = "blesh";
-  version = "0.4.0-devel3";
+  version = "0.4.0-unstable-2026-08-12";
 
-  src = fetchzip {
-    url = "https://github.com/akinomyoga/ble.sh/releases/download/v${version}/ble-${version}.tar.xz";
-    sha256 = "sha256-kGLp8RaInYSrJEi3h5kWEOMAbZV/gEPFUjOLgBuMhCI=";
+  src = fetchFromGitHub {
+    owner = "akinomyoga";
+    repo = "ble.sh";
+    rev = "95ae551dd687a0c61227839dda43f52ac7ea6631";
+    fetchSubmodules = true;
+    hash = "sha256-8yNrsPKF2WB0zZ5B9lTRmYkA0cu8cfBncuLnfcYrmDk=";
+    leaveDotGit = true;
+    postFetch = ''
+      cd $out
+      git archive --format=tar HEAD make/.git-archive-export.mk | tar xf -
+      find "$out" -name .git -type d -print0 | xargs -0 rm -r
+    '';
   };
 
-  dontBuild = true;
+  patches = [ ./no-git-submodule-update.patch ];
+
+  postPatch = ''
+    patchShebangs ./make_command.sh
+  '';
 
   doCheck = true;
   nativeCheckInputs = [
     bashInteractive
     glibcLocales
+    less
+    ps
   ];
   preCheck = "export LC_ALL=en_US.UTF-8";
 
@@ -39,7 +56,7 @@ stdenvNoCC.mkDerivation rec {
     }
     EOF
 
-    cp -rv $src/* $out/share/blesh
+    make install PREFIX=$out
 
     runHook postInstall
   '';
