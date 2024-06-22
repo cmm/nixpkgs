5ac32fb77bde6a9984af103acfc42afe9d7bfd8e
Michael Livshin <repo@cmm.kakpryg.net>
2024-06-22 11:45:08 +0300

    brave: 1.67.116 -> 1.67.119

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -6,17 +6,17 @@ callPackage ./make-brave.nix (removeAttrs args [ "callPackage" ])
     if stdenv.isAarch64 then
       {
         pname = "brave";
-        version = "1.67.116";
-        url = "https://github.com/brave/brave-browser/releases/download/v1.67.116/brave-browser_1.67.116_arm64.deb";
-        hash = "sha256-YOvfXI+bSX3WZRBYXxGYhNme9NZl3ob/JYw6KshXn54=";
+        version = "1.67.119";
+        url = "https://github.com/brave/brave-browser/releases/download/v1.67.119/brave-browser_1.67.119_arm64.deb";
+        hash = "sha256-j0SDqCKyCnS8lAAFya2ueOtWLhfxOJ3VM8b6qBS30h8=";
         platform = "aarch64-linux";
       }
     else if stdenv.isx86_64 then
       {
         pname = "brave";
-        version = "1.67.116";
-        url = "https://github.com/brave/brave-browser/releases/download/v1.67.116/brave-browser_1.67.116_amd64.deb";
-        hash = "sha256-J4ap0bEAg5Q40U155AdAyzereBAo8Q/YDedDVqYvaT4=";
+        version = "1.67.119";
+        url = "https://github.com/brave/brave-browser/releases/download/v1.67.119/brave-browser_1.67.119_amd64.deb";
+        hash = "sha256-6+HIztCwJ2zcvO1pEgHE+WeM6MFf3Ll7xnGtoGhnd3s=";
         platform = "x86_64-linux";
       }
     else
