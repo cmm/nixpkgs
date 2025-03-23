e6f669f6605b05f30180731be9643328cea3953e
Michael Livshin <mlivshin@gmail.com>
2025-03-23 23:08:57 +0200

    add some perl packages

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -16462,6 +16462,21 @@ with self;
     };
   };
 
+  HTTPCacheTransparent = buildPerlPackage {
+    pname = "HTTP-Cache-Transparent";
+    version = "1.4";
+    src = fetchurl {
+      url = "mirror://cpan//authors/id/M/MA/MATTIASH/HTTP-Cache-Transparent-1.4.tar.gz";
+      hash = "sha256-ZiLhteWvlSsXEa6iR73LG5HCqsDxHzL4GaLQfhHdKEU=";
+    };
+    doCheck = false;
+    propagatedBuildInputs = [ LWP ];
+    meta = {
+      description = "Cache the result of http get-requests persistently";
+      license = with lib.licenses; [ artistic2 ];
+    };
+  };
+
   HTTPCookieJar = buildPerlPackage {
     pname = "HTTP-CookieJar";
     version = "0.014";
@@ -18745,6 +18760,19 @@ with self;
     };
   };
 
+  LinguaPreferred = buildPerlPackage {
+    pname = "Lingua-Preferred";
+    version = "0.2.1";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/E/ED/EDAVIS/Lingua-Preferred-0.2.4.tar.gz";
+      hash = "sha256-+ljE+sa2dveMqta0cqeF3QyPpnAEpiKU+8+jo+skPIM=";
+    };
+    meta = {
+      description = "Perl extension to choose a language";
+      license = with lib.licenses; [ artistic1 gpl2Plus ];
+    };
+  };
+
   LinguaPTStemmer = buildPerlPackage {
     pname = "Lingua-PT-Stemmer";
     version = "0.02";
@@ -19659,6 +19687,20 @@ with self;
     };
   };
 
+  LogTraceMessages = buildPerlPackage {
+    pname = "Log-TraceMessages";
+    version = "1.4";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/E/ED/EDAVIS/Log-TraceMessages-1.4.tar.gz";
+      hash = "sha256-NDq0fB1pIA3203vB3QEnEy5vCkabd+5rceyhFGzmz48=";
+    };
+    doCheck = false;
+    meta = {
+      description = "Perl extension for trace messages used in debugging";
+      license = with lib.licenses; [ artistic1 ];
+    };
+  };
+
   MCE = buildPerlPackage {
     pname = "MCE";
     version = "1.889";
@@ -19849,6 +19891,21 @@ with self;
         artistic1
         gpl1Plus
       ];
+    };
+  };
+
+  LWPJSONTiny = buildPerlPackage {
+    pname = "LWP-JSON-Tiny";
+    version = "0.014";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/S/SK/SKINGTON/LWP-JSON-Tiny-0.014.tar.gz";
+      hash = "sha256-5O16U58pZJA6INwSSevGHuovxP3NY1BNKUNkHB1ARKw=";
+    };
+    buildInputs = [ TestFatal JSONMaybeXS ];
+    propagatedBuildInputs = [ LWP ];
+    meta = {
+      description = "use JSON natively with LWP objects";
+      license = with lib.licenses; [ artistic2 ];
     };
   };
 
@@ -37378,6 +37435,20 @@ with self;
         artistic1
         gpl1Plus
       ];
+    };
+  };
+
+  URIEncode = buildPerlModule {
+    pname = "URI-Encode";
+    version = "v1.1.1";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/M/MI/MITHUN/URI-Encode-v1.1.1.tar.gz";
+      hash = "sha256-S7nOTnAWwBOM+cI3VQhZUobvocjcFbRbqkxHKBwIJDs=";
+    };
+    propagatedBuildInputs = [ Encode ];
+    meta = {
+      description = "Simple percent Encoding/Decoding";
+      license = with lib.licenses; [ artistic1 ];
     };
   };
 
