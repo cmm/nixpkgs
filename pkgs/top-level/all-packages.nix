b46933a9af62bab8ab5935ded7c141a6510bc91c
Michael Livshin <mlivshin@gmail.com>
2025-03-23 23:16:21 +0200

    package xmltv

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2531,6 +2531,8 @@ with pkgs;
 
   xmlsort = perlPackages.XMLFilterSort;
 
+  xmltv = callPackage ../by-name/xm/xmltv/package.nix { };
+
   mcelog = callPackage ../os-specific/linux/mcelog {
     util-linux = util-linuxMinimal;
   };
